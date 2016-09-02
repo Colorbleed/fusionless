b5a5328fc42e0e045ef74df6c24948105b570f73
Roy Nieterau <roy_nieterau@hotmail.com>
2016-09-02 15:29:24 +0200

    Improve Fusion 8+ loading and documentation

--- fusionless/standalone.py
+++ fusionless/standalone.py
@@ -1,18 +1,27 @@
 """Initialize a connection with Fusion from a standalone python prompt
 
-The standalone Python still needs access to the Peyeon
+The standalone Python still needs access to the BlackmagicFusion library
+to initialize a connection. For pre-Fusion 8 versions you need PeyeonScript.
 
+Examples:
+
+    # Create a saver in an open fusion in its currently active comp
+    >>> app = get_fusion()
+    >>> comp = app.get_current_comp()
+    >>> saver = comp.create_tool("Saver")
 
 """
 
 try:
-    import PeyeonScript as bmd
-except ImportError:
+    # Fusion 8.0+ (bmd)
     import BlackmagicFusion as bmd
+except ImportError:
+    # Fusion pre-8.0 (eyeon)
+    import PeyeonScript as bmd
     
 
 def _get_app(app, ip=None, timeout=0.1, uuid=None):
-    """
+    """Get an application using the `scriptapp` method.
 
     Args:
         app (str): Name of the application, eg. "Fusion"
@@ -42,8 +51,14 @@ def _get_app(app, ip=None, timeout=0.1, uuid=None):
 def get_fusion(app="Fusion", ip="127.0.0.1", timeout=0.1, uuid=None):
     """Establish connection with an already active Fusion application.
 
+    Args:
+        app (str): Name of the application. (defaults to: "Fusion")
+        ip (str): The IP address of the host to connect to.
+        timeout (float): Timeout for connection in seconds.
+        uuid (str): The UUID of the application to connect to.
+
     Returns:
-        The created Fusion application
+        fusionless.core.Fusion: The created Fusion application
 
     """
     ptr = _get_app(app, ip, timeout, uuid)
